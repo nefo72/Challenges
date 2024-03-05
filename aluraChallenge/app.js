0f7da22f97b7cd39845c22637b722eb1d60e5d2a
Neftalí <56168003+nefo72@users.noreply.github.com>
2024-03-05 11:35:23 -0600

    Primer avance del encriptador

---
+++ aluraChallenge/app.js
@@ -0,0 +1,44 @@
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+
+function botonEncriptar(){
+    const textoEncriptado = encriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none"
+}
+
+
+
+function botonDesencriptar(){
+    const textoEncriptado = desencriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+}
+
+
+
+function encriptar(stringEncriptado){
+    let matrizCodigo =[["e", "enter"],["i","imes"],["a", "ai"],["o", "ober"],["u", "ufat"]];
+    stringEncriptado = stringEncriptado.toLowerCase()
+
+    for(let i = 0; i<matrizCodigo.length; i++){
+        if(stringEncriptado.includes(matrizCodigo[i][0])){
+            stringEncriptado =  stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+    }
+    return stringEncriptado
+}
+
+function desencriptar(stringDesencriptado){
+    let matrizCodigo =[["e", "enter"],["i","imes"],["a", "ai"],["o", "ober"],["u", "ufat"]];
+    stringDesencriptado = stringDesencriptado.toLowerCase()
+
+    for(let i = 0; i<matrizCodigo.length; i++){
+        if(stringDesencriptado.includes(matrizCodigo[i][1])){
+            stringDesencriptado =  stringDesencriptado.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+        }
+    }
+    return stringDesencriptado
+}
+
